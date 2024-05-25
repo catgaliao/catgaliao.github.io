d961b8492d3a42131f6e1eece28b0b7c207002f2
catgaliao <166881182+catgaliao@users.noreply.github.com>
2024-05-25 16:35:10 -0400

    Update and rename action_page.php to sign.php

--- action_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if( isset($_POST['submit']) )
-    {
-        $feature = $_POST['feature'];
-        $feature2 = $_POST['feature2'];
-        // do stuff (will send data to database)
-    }
-?>
