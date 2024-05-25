b8504d6a01a38d08dc754e853a1fd35f93fb32e7
catgaliao <166881182+catgaliao@users.noreply.github.com>
2024-05-25 16:27:24 -0400

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,11 @@
+ <html>
+<head>
+  <title>signup catgaliao</title>
+</head>
+
+<body>
+  <h1>Thanks for joining!</h1>
+  <p>help catgaliao by raising awareness :0, you are one of our first members.</p>
+</body>
+
+</html> 
