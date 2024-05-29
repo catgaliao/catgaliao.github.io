734a08ff77c768ec5f773dee3260c5025605efc7
catgaliao <166881182+catgaliao@users.noreply.github.com>
2024-05-29 08:15:45 -0400

    Update and rename action_page.php to cat.txt

--- action_page.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Retrieve form data
-$name = $_POST["name"];
-$email = $_POST["email"];
-
-// Process the data (you can do more than just echoing)
-echo "Welcome, $name! Your email address is: $email";
